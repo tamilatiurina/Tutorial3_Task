140a45e9baeac36a60ce58a9c2c63e9d10f6a0ed
tiurinatamila <s30844@pjwstk.edu.pl>
2025-04-16 21:36:31 +0200

    Restructured

--- Tutorial3_Task/DeviceStorage.cs
+++
@@ -1,6 +0,0 @@
-namespace Tutorial3_Task;
-
-public static class DeviceStorage
-{
-    //public static DeviceManager _manager = new DeviceManager(@"C:\Users\Home\Downloads\S30844_REST\tutorial3_template\Tutorial3_Task\input.txt");
-}
